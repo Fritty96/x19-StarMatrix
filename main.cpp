79f22e2feec9b5f2e8add9c608ee5218c7ebba21
Fritty96 <mphillips39@mail.csuchico.edu>
2017-10-18 16:38:14 -0700

    Final Stars

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+int main() {
+
+vector <vector<char> > star;
+
+int userrow;
+int usercol;
+
+cout << "Enter rows then columns of stars." << endl;
+cin >> userrow;
+cin >> usercol;
+
+if (userrow <= 0){
+
+}
+else if (usercol <=0){
+}
+
+else{
+star.resize(userrow);
+
+
+for (int row = 0; row < star.size(); row++){
+    star[row].resize(usercol);   
+
+
+    for (int col = 0; col < star[row].size(); col++){
+    cout << "* ";
+    
+    
+    }
+    cout << endl;
+
+}
+}
+
+return 0;
+
+
+}
